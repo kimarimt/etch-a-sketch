b665afba89ab45313a848055dc05069da3ee5fbb
Alec German <kimarimt@protonmail.com>
2022-05-28 16:48:42 -0400

    built etch-a-sketch

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print()
+import turtle
+
+
+def go_forward():
+    t.forward(10)
+
+
+def go_backward():
+    t.backward(10)
+
+
+def turn_counterclockwise():
+    t.setheading(t.heading() + 10)
+
+
+def turn_clockwise():
+    t.setheading(t.heading() - 10)
+
+
+def clear():
+    s.reset()
 
 
 if __name__ == '__main__':
-    main()
+    s = turtle.getscreen()
+    t = turtle.Turtle()
+    t.speed(0)
+    s.listen()
+    s.onkey(go_forward, 'Up')
+    s.onkey(go_backward, 'Down')
+    s.onkey(turn_counterclockwise, 'Right')
+    s.onkey(turn_clockwise, 'Left')
+    s.onkey(clear, 'c')
+    s.mainloop()
